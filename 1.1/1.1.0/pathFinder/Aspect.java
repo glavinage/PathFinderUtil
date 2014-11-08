96f4d21a5088ca0928aefe86daa736fc7f4f220b
glavinage <glavinage@gmail.com>
2014-11-07 21:21:55 -0500

    Added Tooltip to aspect selection buttons in the PathFrame.java file

--- 1.1/1.1.0/pathFinder/Aspect.java
+++ 1.1/1.1.0/pathFinder/Aspect.java
@@ -1,7 +1,10 @@
 package pathFinder;
 
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
-import java.awt.image.*;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Aspect
 {
@@ -11,7 +14,9 @@ public class Aspect
 	private ArrayList<Aspect> compatibleAspects;
 	private BufferedImage image;
 	private boolean enabled;
-	
+	// Counts of total of each Primal Aspect in this aspect
+	int aerCount, aquaCount, ignisCount, ordoCount, perditioCount, terraCount;
+
 	public Aspect(String aspectName, String compA, String compB, BufferedImage aspectImage)
 	{
 		this.name = aspectName;
@@ -21,32 +26,46 @@ public class Aspect
 		this.image = aspectImage;
 		this.enabled = true;
 	}
-	
-	//Returns true if the Partner Aspect is compatible with this aspect
+
+	// Returns true if the Partner Aspect is compatible with this aspect
 	public boolean isValidPartnerAspect(Aspect a)
 	{
-		//If this is a parent Aspect
+		// If this is a parent Aspect
 		if(this.name.equals(a.getComponentA()))
 			return true;
-		//If this is a parent Aspect
+		// If this is a parent Aspect
 		if(this.name.equals(a.getComponentB()))
 			return true;
-		//If this is a child Aspect
+		// If this is a child Aspect
 		if(this.componentA.equals(a.getName()))
 			return true;
-		//If this is a child Aspect
+		// If this is a child Aspect
 		if(this.componentB.equals(a.getName()))
 			return true;
-		
-		//Otherwise not directly related
+
+		// Otherwise not directly related
 		return false;
 	}
-	
-	//Used to generate a list of partner aspects that can link to this Aspect
+
+	// Returns true if the Partner Aspect is compatible with this aspect
+	public boolean isComponentAspect(Aspect a)
+	{
+		// If this is a child Aspect
+		if(this.componentA.equals(a.getName()))
+			return true;
+		// If this is a child Aspect
+		if(this.componentB.equals(a.getName()))
+			return true;
+
+		// Otherwise not directly related
+		return false;
+	}
+
+	// Used to generate a list of partner aspects that can link to this Aspect
 	public void determinePartnerAspects(ArrayList<Aspect> aspectList)
 	{
 		this.compatibleAspects.clear();
-		
+
 		for(int i = 0; i < aspectList.size(); i++)
 		{
 			if(this.isValidPartnerAspect(aspectList.get(i)))
@@ -54,32 +73,91 @@ public class Aspect
 				this.compatibleAspects.add(aspectList.get(i));
 			}
 		}
-		
-		//Save a bit of memory
+
+		// Save a bit of memory
 		this.compatibleAspects.trimToSize();
 	}
-	
-	//Getter Methods
+
+	// Used to calculate the total primal component aspects of this Aspect
+	public void determineComponentAspect(ArrayList<Aspect> aspectList)
+	{
+		Queue<Aspect> q = new LinkedList<Aspect>();
+
+		q.add(this);
+
+		while(!q.isEmpty())
+		{
+			// System.out.println("Test");
+			Aspect a = q.poll();
+
+			if(a.isNamed("Aer"))
+				this.aerCount++;
+			else if(a.isNamed("Aqua"))
+				this.aquaCount++;
+			else if(a.isNamed("Ignis"))
+				this.ignisCount++;
+			else if(a.isNamed("Ordo"))
+				this.ordoCount++;
+			else if(a.isNamed("Perditio"))
+				this.perditioCount++;
+			else if(a.isNamed("Terra"))
+				this.terraCount++;
+			else
+			{
+				for(Aspect t : aspectList)
+				{
+					if(a.isComponentAspect(t))
+					{
+						q.add(t);
+					}
+
+				}
+			}
+		}
+	}
+
+	//
+	public void sortByPrimalCost()
+	{
+		for(int a = 0; a < this.compatibleAspects.size(); a++)
+		{
+			for(int b = a + 1; b < compatibleAspects.size(); b++)
+			{
+				if(this.compatibleAspects.get(b).getTotalPrimalCost() < this.compatibleAspects.get(a)
+						.getTotalPrimalCost())
+				{
+					Collections.swap(this.compatibleAspects, a, b);
+				}
+			}
+		}
+	}
+
+	// Getter Methods
 	public String getName()
 	{
 		return this.name;
 	}
+
 	public String getComponentA()
 	{
 		return this.componentA;
 	}
+
 	public String getComponentB()
 	{
 		return this.componentB;
 	}
+
 	public ArrayList<Aspect> getCompatableAspects()
 	{
 		return this.compatibleAspects;
 	}
+
 	public BufferedImage getImage()
 	{
 		return this.image;
 	}
+
 	public boolean getEnabled()
 	{
 		return this.enabled;
@@ -89,33 +167,65 @@ public class Aspect
 	{
 		this.enabled = !this.enabled;
 	}
-	
-	//Overwrite Functions
+
+	// Overwrite Functions
 	public String toString()
 	{
 		String s = this.name + " is composed of " + this.componentA + " & " + componentB;
-			
+
 		return s;
 	}
-	//Returns a detailed string including all related aspects
+
+	// Returns a detailed string including all related aspects
 	public String toStringDeatil()
 	{
 		String s = this.name + " is composed of " + this.componentA + " & " + componentB + " and is compatible with";
-		
+
 		for(int i = 0; i < this.compatibleAspects.size(); i++)
 		{
 			s += " " + this.compatibleAspects.get(i).getName();
 		}
-		
+
 		return s;
 	}
+
+	// Returns details on what primal components make up this aspect
+	public String toStringBaseComponents()
+	{
+		String s = this.name + " is composed of " + this.aerCount + " Aer " + this.aquaCount + " Aqua "
+				+ this.ignisCount + " Ignis " + this.ordoCount + " Ordo " + this.perditioCount + " Perditio "
+				+ this.terraCount + " Terra for a total of " + this.getTotalPrimalCost();
+
+		return s;
+	}
+
 	public boolean equals(Aspect a)
 	{
 		if(this.getName().equals(a.getName()))
 		{
-            return true;
+			return true;
 		}
-		
+
 		return false;
+	}
+
+	// Returns ture if this aspect has the same name as the passed string
+	public boolean isNamed(String n)
+	{
+		if(this.getName().equals(n))
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+	// Returns total primal cost
+	public int getTotalPrimalCost()
+	{
+		int t = this.aerCount + this.aquaCount + this.ignisCount + this.ordoCount + this.perditioCount
+				+ this.terraCount;
+
+		return t;
 	}
 }
--- 1.1/1.1.0/pathFinder/PathFinder.java
+++ 1.1/1.1.0/pathFinder/PathFinder.java
@@ -1,5 +1,6 @@
 package pathFinder;
 
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -8,7 +9,6 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.awt.image.*;
 
 import javax.imageio.ImageIO;
 
@@ -17,21 +17,21 @@ public class PathFinder
 	private ArrayList<Aspect> allAspectList;
 	private Queue<AspectStep> nodesToCheck;
 	private PathChecker check;
-	
+
 	public PathFinder()
 	{
 		this.allAspectList = new ArrayList<Aspect>();
 		this.nodesToCheck = new LinkedList<AspectStep>();
 		this.check = new PathChecker();
-		
-		//method used to simply constructor
+
+		// method used to simply constructor
 		this.readAspects();
 	}
-	
-	//Should only be called by the constructor
+
+	// Should only be called by the constructor
 	private void readAspects()
 	{
-		//reads in the Research Aspects.csv
+		// reads in the Research Aspects.csv
 		String csvFile = "Research Aspects.csv";
 		String pngFile = "aspects.png";
 		InputStreamReader isr = new InputStreamReader(getClass().getResourceAsStream(csvFile));
@@ -39,108 +39,120 @@ public class PathFinder
 		String line = "";
 		String cvsSplitBy = ",";
 		int aspectNum = 0;
-	 
-		try {
+
+		try
+		{
 			BufferedImage wholeImage = ImageIO.read(new File(pngFile));
-			br = new BufferedReader(isr);;
-			br.readLine();//Ignore Header in .CSV file
-			while ((line = br.readLine()) != null) {
-		        // use comma as separator
+			br = new BufferedReader(isr);
+			;
+			br.readLine();// Ignore Header in .CSV file
+			while((line = br.readLine()) != null)
+			{
+				// use comma as separator
 				String[] aspectTemp = line.split(cvsSplitBy);
-	 
-				//Don't add empty aspects (haven't found them yet...)
-				if (aspectTemp.length > 0)
+
+				// Don't add empty aspects (haven't found them yet...)
+				if(aspectTemp.length > 0)
 				{
-	                int xOffset = aspectNum%8 * 64;
-	                int yOffset = aspectNum/8 * 64;
-	                
-					//reads from the line into the Aspect object
-					this.allAspectList.add(new Aspect(aspectTemp[0], aspectTemp[1], aspectTemp[2], wholeImage.getSubimage(xOffset, yOffset, 64, 64)));
+					int xOffset = aspectNum % 8 * 64;
+					int yOffset = aspectNum / 8 * 64;
+
+					// reads from the line into the Aspect object
+					this.allAspectList.add(new Aspect(aspectTemp[0], aspectTemp[1], aspectTemp[2], wholeImage
+							.getSubimage(xOffset, yOffset, 64, 64)));
 				}
-				
+
 				aspectNum++;
 			}
-	 
-		} catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
+
+		} catch(FileNotFoundException e)
+		{
+			System.out.println(e.getMessage());
 			e.printStackTrace();
-		} catch (IOException e) {
-            System.out.println(e.getMessage());
+		} catch(IOException e)
+		{
+			System.out.println(e.getMessage());
 			e.printStackTrace();
-		} finally {
-			if (br != null) {
-				try {
+		} finally
+		{
+			if(br != null)
+			{
+				try
+				{
 					br.close();
-				} catch (IOException e) {
-		            System.out.println(e.getMessage());
+				} catch(IOException e)
+				{
+					System.out.println(e.getMessage());
 					e.printStackTrace();
 				}
 			}
 		}
-		
-		//Save a bit of memory
+
+		// Save a bit of memory
 		this.allAspectList.trimToSize();
-		
-		//Pre-calculation of partner aspects
-		for (Aspect a : this.allAspectList)
+
+		// Pre-calculation of partner aspects
+		for(Aspect a : this.allAspectList)
 		{
 			a.determinePartnerAspects(this.allAspectList);
+			a.determineComponentAspect(this.allAspectList);
+			a.sortByPrimalCost();
 		}
 	}
-	
+
 	public ArrayList<Aspect> findPath(Aspect start, Aspect end, int minSteps)
 	{
-		//Clears the queue of any old variables
+		// Clears the queue of any old variables
 		this.nodesToCheck.clear();
-		//adds the starting node to the queue
+		// adds the starting node to the queue
 		this.nodesToCheck.add(new AspectStep(start, null));
-                
-                int currentStepNumber = 1;
-                ArrayList<String> stepFoundAspects = new ArrayList<String>();
-		
-		//Initialize the temp variable for the do loop
+
+		int currentStepNumber = 1;
+		ArrayList<String> stepFoundAspects = new ArrayList<String>();
+
+		// Initialize the temp variable for the do loop
 		AspectStep tempStep;
-		
+
 		do
 		{
-                    //grabs the front AspectStep from the queue
-                    tempStep = this.nodesToCheck.poll();
+			// grabs the front AspectStep from the queue
+			tempStep = this.nodesToCheck.poll();
 
-                    if (tempStep == null)
-                        break;
-                    
-                    //If we're on the next step, reset found aspects
-                    if (tempStep.getStepNumber() > currentStepNumber)
-                    {
-                        stepFoundAspects.clear();
-                        currentStepNumber++;
-                    }
+			if(tempStep == null)
+				break;
 
-                    for(Aspect a : tempStep.getCurrentAspect().getCompatableAspects())
-                    {
-                        if (a.getEnabled() && !stepFoundAspects.contains(a.getName()))
-                        {
-                            //Adds a new node to check to the queue for each possible path
-                            this.nodesToCheck.add(new AspectStep(a, tempStep));
+			// If we're on the next step, reset found aspects
+			if(tempStep.getStepNumber() > currentStepNumber)
+			{
+				stepFoundAspects.clear();
+				currentStepNumber++;
+			}
 
-                            stepFoundAspects.add(a.getName());
-                        }
-                    }
-			
-		//loop repeats until the path is valid
-		}while(!this.check.checkPathWorks(start, end, minSteps, tempStep));
-		
-                if (tempStep != null)
-                {
-                    //returns the valid path based on the tempStep's recursive pointers
-                    return this.check.getSolution(tempStep);
-                }
-                else
-                {
-                    return null;
-                }
+			for(Aspect a : tempStep.getCurrentAspect().getCompatableAspects())
+			{
+				if(a.getEnabled() && !stepFoundAspects.contains(a.getName()))
+				{
+					// Adds a new node to check to the queue for each possible
+					// path
+					this.nodesToCheck.add(new AspectStep(a, tempStep));
+
+					stepFoundAspects.add(a.getName());
+				}
+			}
+
+			// loop repeats until the path is valid
+		} while(!this.check.checkPathWorks(start, end, minSteps, tempStep));
+
+		if(tempStep != null)
+		{
+			// returns the valid path based on the tempStep's recursive pointers
+			return this.check.getSolution(tempStep);
+		} else
+		{
+			return null;
+		}
 	}
-	
+
 	public ArrayList<Aspect> getAllAspectList()
 	{
 		return this.allAspectList;
